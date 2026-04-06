49d48c869cb8b2d2c2bb6f2734fd6737a2d6badc
chautdat <chautandat@chautandats-MacBook-Pro.local>
2026-04-06 10:02:12 +0700

    Cập nhật entity Address, Category và Product

--- src/main/java/com/nhom1ck/webdatdoan/entity/Category.java
+++ src/main/java/com/nhom1ck/webdatdoan/entity/Category.java
@@ -2,10 +2,17 @@ package com.nhom1ck.webdatdoan.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "categories")
--- src/main/java/com/nhom1ck/webdatdoan/entity/Product.java
+++ src/main/java/com/nhom1ck/webdatdoan/entity/Product.java
@@ -3,10 +3,19 @@ package com.nhom1ck.webdatdoan.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "products")
